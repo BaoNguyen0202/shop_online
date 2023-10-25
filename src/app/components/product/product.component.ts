afaf204f3bcadc29dcdde17b87bd7a9243b1ab69
dev1 <bao17030202@gmail.com>
2023-10-25 22:42:07 +0700

    +search

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -8,13 +8,29 @@ import { ProductService } from 'src/app/product.service';
 })
 export class ProductComponent implements OnInit {
   products: any[] = [];
+  filteredProducts: any[] = [];
 
   constructor(private productService: ProductService) { }
 
   ngOnInit() {
     this.productService.getProducts().subscribe((data: any) => {
       this.products = data.products;
+      this.filteredProducts = [...this.products];
       console.log(this.products);
     });
   }
+
+  onSearchInput(event: Event) {
+    const inputValue = (event.target as HTMLInputElement).value;
+  
+    this.filteredProducts = this.products.filter(product => {
+      const productName = product.name.toLowerCase();
+      const productPrice = product.price.toString().toLowerCase();
+      const searchTerm = inputValue.toLowerCase();
+  
+      return productName.includes(searchTerm) || productPrice.includes(searchTerm);
+    });
+  }
+  
+  
 }
